88c479e8573dc2c76cfe473dd2ce1e76390f276e
ThenBuzzard100 <thenbuzzard100@gmail.com>
2026-04-07 17:33:50 -0600

    Update GameManager.cs

--- Assets/Scripts/Core/GameManager.cs
+++ Assets/Scripts/Core/GameManager.cs
@@ -3,7 +3,7 @@ using System.Collections.Generic;
 using UnityEngine;
 using UnityEngine.SceneManagement;
 
-namespace MimicFacility.Core
+public namespace GameManager : MonoBehavior;
 {
     public enum EGamePhase
     {
